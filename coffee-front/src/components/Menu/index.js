d2aca743a51ba7047494975c104f0178bbfe5778
Soufi-Silva <sofiavictoriasm14@gmail.com>
2024-06-17 14:07:03 -0400

    front

--- coffee-front/src/components/Menu/index.js
+++ coffee-front/src/components/Menu/index.js
@@ -3,26 +3,26 @@ import '../../Styles/menu.css';
 import logo from '../../image/6.png';
 
 
-
 function Menu(){
     return <>
-        <ul className="menu">
-        <div className="logo-container">
-            <img src={logo} alt="" className="logo" />
-         </div>
-            {
-             routes.map( (item, index)=>(
-                <li key={index}>
-                    <NavLink 
-                        style={({isActive}) => ({color:isActive?"gray":"white"})}
-                        to={item.to}>
-                        {item.text}
-                    </NavLink>
-                </li>
-             ) )
-            }
-        </ul>
-
+        <div className="container-barra">
+            <div className="logo-container">
+                <img src={logo} alt="" className="logo" />
+            </div>
+            <ul className="menu">
+                {
+                routes.map( (item, index)=>(
+                    <li key={index}>
+                        <NavLink 
+                            style={({isActive}) => ({color:isActive?"gray":"white"})}
+                            to={item.to}>
+                            {item.text}
+                        </NavLink>
+                    </li>
+                ) )
+                }
+            </ul>
+        </div>
     </>
 }
 
